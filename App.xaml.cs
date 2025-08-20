41f04c8d7c9d2a12931fb370171e1e0b892c3615
Dmitry K <sieger1012@gmail.com>
2025-08-20 13:23:08 +0700

    Добавление иконки для приложения

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,12 @@
-﻿using System.Configuration;
+﻿using PasswordListManager.Models;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace PasswordListManager
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+{    
     public partial class App : Application
     {
+       
     }
-
 }
